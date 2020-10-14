d26bb127657536faec4ebe22ba84bc6106050d7f
jingisium <jingisium@gmail.com>
2020-10-14 14:59:52 +0900

    planモデル作成

--- app/javascript/calendar.js
+++ app/javascript/calendar.js
@@ -1,11 +1,14 @@
 import { Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
+import allLocales from '@fullcalendar/core/locales-all.js';
 
 function calendar(){
   var calendarEl = document.getElementById('calendar');
 
   var calendar = new Calendar(calendarEl, {
+    locales: allLocales,
+    locale: 'ja',
     plugins: [ dayGridPlugin, interactionPlugin ]
   });
 
