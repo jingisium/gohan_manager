e389a5f47f3963c725bb10f0d162403076cdd024
jingisium <jingisium@gmail.com>
2020-10-21 17:13:45 +0900

    投稿内容のカレンダー表示

--- app/javascript/calendar.js
+++ app/javascript/calendar.js
@@ -9,7 +9,40 @@ window.addEventListener('turbolinks:load', () => {
   var calendar = new Calendar(calendarEl, {
     locales: allLocales,
     locale: 'ja',
-    plugins: [ dayGridPlugin, interactionPlugin ]
+    contentHeight: 'auto',
+    plugins: [ dayGridPlugin, interactionPlugin ],
+    headerToolbar: {
+      start: 'prev',
+      center: 'title',
+      end: 'today next'
+    },
+
+    dayCellContent: function(e) {
+      e.dayNumberText = e.dayNumberText.replace('日', '');
+    },
+
+    events: events,
+
+    eventContent: function(plan) {
+      var arrayOfDomNodes = [];
+      
+      var titleEvent = document.createElement('div');
+      if(plan.event._def.title) {
+        titleEvent.innerHTML = plan.event._def.title;
+        titleEvent.classList = "fc-event-title fc-sticky";
+      };
+
+      var imgEventWrap = document.createElement('div');
+      if(plan.event.extendedProps.image_url) {
+        var imgEvent = '<img src="'+plan.event.extendedProps.image_url+'" class="w-100 h-75" >';
+        imgEventWrap.classList = "fc-event-img";
+        imgEventWrap.innerHTML = imgEvent;
+      };
+
+      arrayOfDomNodes = [ titleEvent,imgEventWrap ];
+
+      return { domNodes: arrayOfDomNodes };
+    },
   });
 
   calendar.render();
